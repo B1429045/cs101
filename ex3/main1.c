2c7369d73d9ae4208592aa639357658759326577
Dwain <dwainphionex@gmail.com>
2025-10-02 14:13:32 +0800

    Create main1.c

---
+++ ex3/main1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n = 8;
+    int i = 1;
+    if (i = (n % 2)){
+        printf("false");
+    }
+    
+    else {
+        printf("true");
+    }
+    return 0;
+}
